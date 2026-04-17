72c126cf0f8ffe1bb38b0e5c56eb597a92b6a49d
Pumsheen <meghanlqt@gmail.com>
2026-04-17 15:15:12 +0800

    Update home route to return JSON response

--- server.py
+++ server.py
@@ -4,9 +4,9 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/')
+@app.route("/", methods=["GET"])
 def home():
-    return "Server is running"
+    return jsonify({"status": "server running"})
 
 @app.route('/bus', methods=['POST'])
 def receive_bus():

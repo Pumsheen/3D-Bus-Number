6befb21ed2f25969f5330521028d4bfde4ca40b0
Pumsheen <meghanlqt@gmail.com>
2026-04-17 13:23:16 +0800

    Add /bus route

--- server.py
+++ server.py
@@ -4,6 +4,10 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+@app.route('/')
+def home():
+    return "Server is running"
+
 @app.route('/bus', methods=['POST'])
 def receive_bus():
     data = request.json
